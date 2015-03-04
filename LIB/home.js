4a0ba14638f650dd91c5690f83d31581c2144130
i306517 <gaopeng.chen@sap.com>
2015-03-04 17:50:57 +0800

    Add "You don't know javascript" and Enhance template generator

--- LIB/home.js
+++ LIB/home.js
@@ -5,6 +5,8 @@
 
     var regex = {
         /*
+         * comments is a regex can check javascript comments.
+         *
          * Please ref: 
          * http://stackoverflow.com/questions/1979884/how-to-use-javascript-regex-over-multiple-lines
          * http://jsperf.com/javascript-multiline-regexp-workarounds
@@ -26,9 +28,77 @@
             }
         }
     });
+
+    function analysisScriptsTest(customizedScript){
+        var comments = customizedScript.match(regex.comments);
+        var codes = customizedScript.split(regex.comments);
+    }
     
+    function analysisScripts(customizedScript){
+        var comments = customizedScript.match(regex.comments);
+        comments.forEach(function(comment, index, theArray){
+            theArray[index] = comment.slice(2, -2).trim();
+        });
+        var codes = customizedScript.split(regex.comments);
+        //codes = codes.filter(function(item){
+        //    return item.trim() !== ""; 
+        //});
+
+        appendScripts(comments, codes);
+    }
+
+    function appendScripts(titles, codes){        
+        //if(titles.length !== codes.length){
+        //    return;
+        //}
+
+        var html = "";
+
+        for ( var i = 0, j = codes.length; i < j; i++){
+            var title = titles[i];
+            var code = codes[i];
+
+            var codeHtml = "", 
+                titleHtml = "";
+            
+            if(code !== undefined && code.trim() !== ""){
+                codeHtml = "<pre class='codePanel prettyprint lang-js'><code>{1}</code></pre>";
+                codeHtml = codeHtml.replace("{1}", code);
+                html += codeHtml;
+            }
+
+            if(title !== undefined && title.trim() !== ""){
+                titleHtml = "<pre class='content'>{0}</pre>";
+                titleHtml = titleHtml.replace("{0}", title);
+                html += titleHtml;
+            }
+        }
+        $("#container").append(html);
+    }
 
     //build scroll button and bind event
+    function buildScrollButton(){
+        //update for on top button
+	    var box = document.createElement("div");
+	    var inner = document.createElement("div");
+	    var smallbox = document.createElement("div");
+	    box.id = "scroll";
+	    var eWarp = document.getElementById("container");
+	    eWarp.appendChild(box);
+	    //var script = document.getElementsByTagName("script")[0];
+	    //document.body.insertBefore(box, script);
+	    if (!window.XMLHttpRequest) {
+	    	box.style.cssText = "_position:absolute;background-color:#555;width:40px;height:40px;right:50px;bottom:115px;cursor:pointer;display:none;";
+	    } else {
+	    	box.style.cssText = "background-color:#555;width:40px;height:40px;position:fixed;right:50px;bottom:115px;cursor:pointer;display:none;opacity: 0.7;";
+	    }
+	    inner.style.cssText = "height: 0;width: 0;border:14px solid #555;border-top: 0 none;border-bottom:14px solid #fff;position: relative;margin:8px 0 0 6px;";
+	    smallbox.style.cssText = "width:8px;height:7px;position:absolute;top:14px;left:-4px;background-color:#fff;overflow: hidden;";
+	    document.body.style.cssText = "background-image:url(about:blank);background-attachment:fixed;";
+	    box.appendChild(inner);
+	    inner.appendChild(smallbox);
+    }
+
     buildScrollButton();
     var scroll = document.getElementById("scroll");
     var timer = null;
@@ -51,7 +121,7 @@
 		clearInterval(timer);
 		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 		timer = setInterval(function() {
-			var speed = Math.floor(-scrollTop / 10);
+			var speed = Math.floor(-scrollTop / 5);
 			scrollTop = scrollTop + speed;
 			
 			document.documentElement.scrollTop = document.body.scrollTop = scrollTop;
@@ -60,56 +130,4 @@
 			}
 		}, 30);
 	};
-
-    function analysisScripts(customizedScript){
-        var comments = customizedScript.match(regex.comments);
-        comments.forEach(function(comment, index, theArray){
-            theArray[index] = comment.slice(2, -2).trim();
-        });
-        var codes = customizedScript.split(regex.comments);
-        codes = codes.filter(function(item){
-            return item.trim() !== ""; 
-        });
-
-        appendScripts(comments, codes);
-    }
-
-    function appendScripts(titles, codes){        
-        if(titles.length !== codes.length){
-            return;
-        }
-
-        for ( var i = 0, j = titles.length; i < j; i++){
-            var title = titles[i];
-            var code = codes[i];
-            
-            var html = "<pre><strong>{0}</strong></pre><pre class = 'codePanel prettyprint lang-js'><code>{1}</code></pre>";
-            html = html.replace("{0}", title);
-            html = html.replace("{1}", code);
-                 
-            $("#container").append(html);
-        }
-    }
-
-    function buildScrollButton(){
-        //update for on top button
-	    var box = document.createElement("div");
-	    var inner = document.createElement("div");
-	    var smallbox = document.createElement("div");
-	    box.id = "scroll";
-	    var eWarp = document.getElementById("container");
-	    eWarp.appendChild(box);
-	    //var script = document.getElementsByTagName("script")[0];
-	    //document.body.insertBefore(box, script);
-	    if (!window.XMLHttpRequest) {
-	    	box.style.cssText = "_position:absolute;background-color:#555;width:40px;height:40px;right:50px;bottom:115px;cursor:pointer;display:none;";
-	    } else {
-	    	box.style.cssText = "background-color:#555;width:40px;height:40px;position:fixed;right:50px;bottom:115px;cursor:pointer;display:none;opacity: 0.7;";
-	    }
-	    inner.style.cssText = "height: 0;width: 0;border:14px solid #555;border-top: 0 none;border-bottom:14px solid #fff;position: relative;margin:8px 0 0 6px;";
-	    smallbox.style.cssText = "width:8px;height:7px;position:absolute;top:14px;left:-4px;background-color:#fff;overflow: hidden;";
-	    document.body.style.cssText = "background-image:url(about:blank);background-attachment:fixed;";
-	    box.appendChild(inner);
-	    inner.appendChild(smallbox);
-    }
 }());
