f5699c6257c995d60b853250409ac3b36dc4b7c7
i306517 <gaopeng.chen@sap.com>
2015-06-08 11:29:56 +0800

    new note and vim config

--- LIB/home.js
+++ LIB/home.js
@@ -1,4 +1,4 @@
-(function(){
+(function() {
     'use strict';
 
     var my = {};
@@ -7,32 +7,32 @@
         /*
          * comments is a regex can check javascript comments.
          *
-         * Please ref: 
+         * Please ref:
          * http://stackoverflow.com/questions/1979884/how-to-use-javascript-regex-over-multiple-lines
          * http://jsperf.com/javascript-multiline-regexp-workarounds
          *
-         * comments : /\/\*.*?\*\//g 
-         * comments : /\/\*[\s\S]*?\*\//g 
+         * comments : /\/\*.*?\*\//g
+         * comments : /\/\*[\s\S]*?\*\//g
          *
          * E.g. \/* abc  *\/
          */
-        comments : /\/\*[^]*?\*\//g  
+        comments: /\/\*[^]*?\*\//g
     };
 
-    $(window).load(function(){
+    $(window).load(function() {
         var scripts = document.scripts;
-        for(var i = 0, j = scripts.length; i < j; i++){
-            if(scripts[i].type === "text/javascript"){
-                var customizedScript = scripts[i].innerHTML.trim();    
+        for (var i = 0, j = scripts.length; i < j; i++) {
+            if (scripts[i].type === "text/javascript") {
+                var customizedScript = scripts[i].innerHTML.trim();
                 analysisScripts(customizedScript);
             }
         }
     });
-    
-    function analysisScripts(customizedScript){
+
+    function analysisScripts(customizedScript) {
         var comments = customizedScript.match(regex.comments);
-        if(comments && comments.length > 0){
-            comments.forEach(function(comment, index, theArray){
+        if (comments && comments.length > 0) {
+            comments.forEach(function(comment, index, theArray) {
                 theArray[index] = comment.slice(2, -2).trim();
             });
         } else {
@@ -46,27 +46,27 @@
         appendScripts(comments, codes);
     }
 
-    function appendScripts(titles, codes){        
+    function appendScripts(titles, codes) {
         //if(titles.length !== codes.length){
         //    return;
         //}
 
         var html = "";
 
-        for ( var i = 0, j = codes.length; i < j; i++){
+        for (var i = 0, j = codes.length; i < j; i++) {
             var title = titles[i];
             var code = codes[i];
 
-            var codeHtml = "", 
+            var codeHtml = "",
                 titleHtml = "";
-            
-            if(code !== undefined && code.trim() !== ""){
+
+            if (code !== undefined && code.trim() !== "") {
                 codeHtml = "<pre class='codePanel prettyprint lang-js'><code class='language-js'>{1}</code></pre>";
                 codeHtml = codeHtml.replace("{1}", code);
                 html += codeHtml;
             }
 
-            if(title !== undefined && title.trim() !== ""){
+            if (title !== undefined && title.trim() !== "") {
                 titleHtml = "<pre class='content'>{0}</pre>";
                 titleHtml = titleHtml.replace("{0}", title);
                 html += titleHtml;
@@ -76,57 +76,57 @@
     }
 
     //build scroll button and bind event
-    function buildScrollButton(){
+    function buildScrollButton() {
         //update for on top button
-	    var box = document.createElement("div");
-	    var inner = document.createElement("div");
-	    var smallbox = document.createElement("div");
-	    box.id = "scroll";
-	    var eWarp = document.getElementById("container");
-	    eWarp.appendChild(box);
-	    //var script = document.getElementsByTagName("script")[0];
-	    //document.body.insertBefore(box, script);
-	    if (!window.XMLHttpRequest) {
-	    	box.style.cssText = "_position:absolute;background-color:#555;width:40px;height:40px;right:50px;bottom:115px;cursor:pointer;display:none;";
-	    } else {
-	    	box.style.cssText = "background-color:#555;width:40px;height:40px;position:fixed;right:50px;bottom:115px;cursor:pointer;display:none;opacity: 0.7;";
-	    }
-	    inner.style.cssText = "height: 0;width: 0;border:14px solid #555;border-top: 0 none;border-bottom:14px solid #fff;position: relative;margin:8px 0 0 6px;";
-	    smallbox.style.cssText = "width:8px;height:7px;position:absolute;top:14px;left:-4px;background-color:#fff;overflow: hidden;";
-	    document.body.style.cssText = "background-image:url(about:blank);background-attachment:fixed;";
-	    box.appendChild(inner);
-	    inner.appendChild(smallbox);
+        var box = document.createElement("div");
+        var inner = document.createElement("div");
+        var smallbox = document.createElement("div");
+        box.id = "scroll";
+        var eWarp = document.getElementById("container");
+        eWarp.appendChild(box);
+        //var script = document.getElementsByTagName("script")[0];
+        //document.body.insertBefore(box, script);
+        if (!window.XMLHttpRequest) {
+            box.style.cssText = "_position:absolute;background-color:#555;width:40px;height:40px;right:50px;bottom:115px;cursor:pointer;display:none;";
+        } else {
+            box.style.cssText = "background-color:#555;width:40px;height:40px;position:fixed;right:50px;bottom:115px;cursor:pointer;display:none;opacity: 0.7;";
+        }
+        inner.style.cssText = "height: 0;width: 0;border:14px solid #555;border-top: 0 none;border-bottom:14px solid #fff;position: relative;margin:8px 0 0 6px;";
+        smallbox.style.cssText = "width:8px;height:7px;position:absolute;top:14px;left:-4px;background-color:#fff;overflow: hidden;";
+        document.body.style.cssText = "background-image:url(about:blank);background-attachment:fixed;";
+        box.appendChild(inner);
+        inner.appendChild(smallbox);
     }
 
     buildScrollButton();
     var scroll = document.getElementById("scroll");
     var timer = null;
-	window.onscroll = window.onmousewheel = document.onmousewheel = function() {
-		//compatibility verify 
-		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-		if (scrollTop > 1000) {
-			scroll.style.display = "block";
-		} else {
-			scroll.style.display = "none";
-		}
-		//implement (position: fixed) in IE browser -> using viewport height minus element height.
-		//keep element at the absolute place in viewport
-		if (!window.XMLHttpRequest) {
-			var T = document.documentElement.clientHeight - scroll.offsetHeight - 20;
-			scroll.style.top = scrollTop + T;
-		}
-	};
-	scroll.onclick = function() {
-		clearInterval(timer);
-		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-		timer = setInterval(function() {
-			var speed = Math.floor(-scrollTop / 5);
-			scrollTop = scrollTop + speed;
-			
-			document.documentElement.scrollTop = document.body.scrollTop = scrollTop;
-			if (scrollTop === 0) {
-				clearInterval(timer);
-			}
-		}, 30);
-	};
+    window.onscroll = window.onmousewheel = document.onmousewheel = function() {
+        //compatibility verify 
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        if (scrollTop > 1000) {
+            scroll.style.display = "block";
+        } else {
+            scroll.style.display = "none";
+        }
+        //implement (position: fixed) in IE browser -> using viewport height minus element height.
+        //keep element at the absolute place in viewport
+        if (!window.XMLHttpRequest) {
+            var T = document.documentElement.clientHeight - scroll.offsetHeight - 20;
+            scroll.style.top = scrollTop + T;
+        }
+    };
+    scroll.onclick = function() {
+        clearInterval(timer);
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        timer = setInterval(function() {
+            var speed = Math.floor(-scrollTop / 5);
+            scrollTop = scrollTop + speed;
+
+            document.documentElement.scrollTop = document.body.scrollTop = scrollTop;
+            if (scrollTop === 0) {
+                clearInterval(timer);
+            }
+        }, 30);
+    };
 }());
---
+++ _vimrc
@@ -0,0 +1,160 @@
+filetype off                  " required
+syn on "syntax on
+colorscheme desert "set theme 
+
+
+
+
+" -- Vundle --
+set nocompatible              " be iMproved, required
+
+" set the runtime path to include Vundle and initialize
+set rtp+=~/vimfiles/bundle/Vundle.vim/
+let path='~/vimfiles/bundle'
+call vundle#begin(path)
+" alternatively, pass a path where Vundle should install plugins
+"call vundle#begin('~/some/path/here')
+
+" let Vundle manage Vundle, required
+Plugin 'gmarik/Vundle.vim'
+Plugin 'Syntastic'
+Plugin 'maksimr/vim-jsbeautify'
+Plugin 'einars/js-beautify'
+Plugin 'scrooloose/nerdtree.git'
+" The following are examples of different formats supported.
+" Keep Plugin commands between vundle#begin/end.
+" plugin on GitHub repo
+"   Plugin 'tpope/vim-fugitive'
+" plugin from http://vim-scripts.org/vim/scripts.html
+"   Plugin 'L9'
+" Git plugin not hosted on GitHub
+"   Plugin 'git://git.wincent.com/command-t.git'
+" git repos on your local machine (i.e. when working on your own plugin)
+"   Plugin 'file:///home/gmarik/path/to/plugin'
+" The sparkup vim script is in a subdirectory of this repo called vim.
+" Pass the path to set the runtimepath properly.
+"   Plugin 'rstacruz/sparkup', {'rtp': 'vim/'}
+" Avoid a name conflict with L9
+"   Plugin 'user/L9', {'name': 'newL9'}
+
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin indent on    " required
+" To ignore plugin indent changes, instead use:
+"filetype plugin on
+"
+" Brief help
+" :PluginList       - lists configured plugins
+" :PluginInstall    - installs plugins; append `!` to update or just :PluginUpdate
+" :PluginSearch foo - searches for foo; append `!` to refresh local cache
+" :PluginClean      - confirms removal of unused plugins; append `!` to auto-approve removal
+"
+" see :h vundle for more details or wiki for FAQ
+" Put your non-Plugin stuff after this line
+
+
+
+
+" -- Common --
+set ai
+set bs=2
+set showmatch
+set expandtab
+set cursorline
+set number
+set autoread
+set ignorecase
+set fileencodings=utf-8,gbk
+set nocompatible              
+set hls
+set ruler "Display cursor coordinate at the right-bottom corner
+set ambiwidth=double
+set guifont=Consolas
+set history=100
+set softtabstop=4
+set smartindent
+set smartcase
+set autoindent shiftwidth=4
+set laststatus=2
+set tabstop=4
+
+"Folder
+set foldenable
+set fdm=indent "foldmethod
+set foldnestmax=6
+"set foldopen=all
+"set foldclose=all
+
+"Avoid auto wrap
+set wrap
+set linebreak
+set nolist
+set textwidth=0 
+set wrapmargin=0
+"set formatoptions, formatoptions need to be reset after open NERDTree
+set fo-=t
+
+"set backup and backup directory
+set backup
+set backupdir=C:\Gaopeng.Chen\VIMbackup
+set directory=C:\Gaopeng.Chen\VIMbackup
+
+
+
+
+
+" -- Syntastic --
+set statusline+=%#warningmsg#
+set statusline+=%{SyntasticStatuslineFlag()}
+set statusline+=%*
+
+let g:syntastic_always_populate_loc_list = 1
+let g:syntastic_auto_loc_list = 1
+let g:syntastic_loc_list_height=5
+let g:syntastic_check_on_open = 1
+let g:syntastic_check_on_wq = 0
+let g:syntastic_javascript_checkers = ['jshint']
+let g:syntastic_javascript_jshint_exec='C:\Users\I306517\AppData\Roaming\npm\jshint.cmd'
+let g:syntastic_javascript_jshint_args='--config "C:\Gaopeng.Chen\workspace\hana_epm_fpa\config\coding\jshintConfigUI.js"'
+
+
+" -- NERDTree config --
+"F2 to toggle NERDTree
+map <F2> :NERDTreeToggle<CR> 
+autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTreeType") &&b:NERDTreeType == "primary") | q | endif
+"Auto open NERDTree
+"autocmd vimenter * NERDTree C:\Gaopeng.Chen\workspace\hana_epm_fpa\fpa-app\src\sap\fpa\ui\scripts
+"Let NERDTree show bookmarks always.
+let NERDTreeShowBookmarks=1 
+"Auto set VIM pwd directory as default when open bookmarks
+let NERDTreeChDirMode=2
+
+source $VIMRUNTIME/vimrc_example.vim
+source $VIMRUNTIME/mswin.vim
+behave mswin
+
+set diffexpr=MyDiff()
+function MyDiff()
+  let opt = '-a --binary '
+  if &diffopt =~ 'icase' | let opt = opt . '-i ' | endif
+  if &diffopt =~ 'iwhite' | let opt = opt . '-b ' | endif
+  let arg1 = v:fname_in
+  if arg1 =~ ' ' | let arg1 = '"' . arg1 . '"' | endif
+  let arg2 = v:fname_new
+  if arg2 =~ ' ' | let arg2 = '"' . arg2 . '"' | endif
+  let arg3 = v:fname_out
+  if arg3 =~ ' ' | let arg3 = '"' . arg3 . '"' | endif
+  let eq = ''
+  if $VIMRUNTIME =~ ' '
+    if &sh =~ '\<cmd'
+      let cmd = '""' . $VIMRUNTIME . '\diff"'
+      let eq = '"'
+    else
+      let cmd = substitute($VIMRUNTIME, ' ', '" ', '') . '\diff"'
+    endif
+  else
+    let cmd = $VIMRUNTIME . '\diff'
+  endif
+  silent execute '!' . cmd . ' ' . opt . arg1 . ' ' . arg2 . ' > ' . arg3 . eq
+endfunction
+
